31896d4dd5fced6c227968659e7d6b557c73667e
peytonfloyd87-droid <peyton.floyd87@gmail.com>
2026-01-19 15:54:28 -0600

    bk

--- influenceforge-web/app/checkout/page.tsx
+++ influenceforge-web/app/checkout/page.tsx
@@ -1,10 +1,6 @@
-import { Suspense } from "react";
 import CheckoutClient from "./CheckoutClient";
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div style={{ padding: 24 }}>Loading checkout...</div>}>
-      <CheckoutClient />
-    </Suspense>
-  );
+  return <CheckoutClient />;
 }
+
